90272a17daed60075450390eb3f6f3f35e3edbfe
toledo-luanda <luandatoledoviera@outlook.es>
2023-05-23 22:33:18 -0300

    v 0.02

--- primer_parcial/primer_parcial.py
+++ primer_parcial/primer_parcial.py
@@ -36,7 +36,25 @@ mostrar_jugadores(dicc_jugadores)
 # incluyendo temporadas jugadas, puntos totales, promedio de puntos por partido, rebotes totales, 
 # promedio de rebotes por partido, asistencias totales, promedio de asistencias por partido, robos totales, 
 # bloqueos totales, porcentaje de tiros de campo, porcentaje de tiros libres y porcentaje de tiros triples.
+def mostrar_segun_indice(dicc_jugadores):
+    
+    indice_del_jugador = int(input("\nIngrese el indice del jugador: "))
+    if indice_del_jugador < 0 or indice_del_jugador >= len(dicc_jugadores):
+        imprimir_dato("Indice de jugador invalido.")
+    else:
+        jugador = dicc_jugadores[indice_del_jugador]
+        nombre = jugador["nombre"]
+        posicion = jugador["posicion"]
+        estadisticas = jugador["estadisticas"]
 
+        imprimir_dato(f"Nombre: {nombre} - Posicion: {posicion} - Estadisticas: ")
+
+        for clave, valor in estadisticas.items():
+            mensaje = f"»{clave}: {valor}"
+            mensaje_formateado = mensaje.replace("_", " ")
+            imprimir_dato(mensaje_formateado)
+
+mostrar_segun_indice(dicc_jugadores)
 
 
 
